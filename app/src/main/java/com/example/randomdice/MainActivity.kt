34860e4a96120ad779e108603d288d4321e3c1d4
Zheng Wei <65906926+de-only-wei@users.noreply.github.com>
2023-02-20 13:25:25 -0800

    Initial and final commit

---
+++ app/src/main/java/com/example/randomdice/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.randomdice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+fun main() {
+    val myFirstDice = Dice();
+    println(myFirstDice.sides)
+    val diceRoll = myFirstDice.roll();
+}
+
+class Dice{
+    var sides = 6;
+
+    fun roll() {
+        val randomNumber = (1..6).random();
+        println(randomNumber);
+
+
+    }
+
+}
